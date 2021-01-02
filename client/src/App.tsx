5688594446574b3fbdf7e9825beba24b791e10ec
Alex Pirciu <alex.pirciu@zohomail.eu>
2021-01-02 19:09:18 +0200

    Create the basic structure of the client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img src={logo} alt="SpaceX logo" />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+                numquam quod at ullam! Autem ut sed voluptas ex velit et magni
+                iure nam ea est eveniet vero, corporis odit veniam!
+            </p>
+        </div>
+    );
 }
 
 export default App;
