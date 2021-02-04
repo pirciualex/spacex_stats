3c60a63bbcbabb873c9ce4276a66422f09e82fc7
Alex Pirciu <alex.pirciu@zohomail.eu>
2021-02-05 00:32:19 +0200

    Add deploy config

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from "apollo-server-express";
 import cors from "cors";
 import express from "express";
 import { __port__ } from "./constants";
+import path from "path"
 import schema from "./graphql/schema";
 
 const main = () => {
@@ -14,6 +15,12 @@ const main = () => {
     });
 
     apolloServer.applyMiddleware({ app });
+
+    app.use(express.static("public"))
+
+    app.get("*",(_req,res)=>{
+        res.sendFile(path.resolve(__dirname,"public", "index.html"))
+    })
 
     app.listen(__port__, () => {
         console.log(`server started on port ${__port__}`);
